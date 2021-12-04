9cd482eabc23bbd03f8fc33fa0e4c98314290b6f
HosseinZadeh <mrhb6006@gmail.com>
2021-12-04 10:05:55 +0330

    init mainController for commonError and response

--- controllers/mainController/controller.go
+++ controllers/mainController/controller.go
@@ -1,1 +1,91 @@
 package mainController
+
+import (
+	"cloudProject/apiSchema/common"
+	"cloudProject/pkg/cast"
+	"cloudProject/pkg/prometheus"
+	"cloudProject/pkg/utils"
+	translate "cloudProject/statics/translate/message"
+	"github.com/gofiber/fiber/v2"
+	jsoniter "github.com/json-iterator/go"
+	"strconv"
+)
+
+var serviceStats map[string]*prometheus.Stats
+
+type Request interface {
+	Validate(ctx *fiber.Ctx) (string, int, error)
+}
+
+func ParseBody(ctx *fiber.Ctx, request Request) (string, int, error) {
+	err := jsoniter.Unmarshal(ctx.Body(), request)
+	if err != nil {
+		return "02", 400, err
+	}
+	return request.Validate(ctx)
+}
+func prepareBody(ctx *fiber.Ctx, body interface{}) error {
+	data, err := jsoniter.Marshal(body)
+	if err != nil {
+		return err
+	}
+	_, _ = ctx.Write(data)
+	return nil
+}
+func Response(ctx *fiber.Ctx, data interface{}) error {
+	res := common.Response{}
+	lang := GetAcceptLanguage(ctx)
+	res.Status = "success"
+	message := ""
+	if utils.IsNonEmptyString(data) {
+		message, _ = cast.ToString(data)
+	}
+
+	if message != "" {
+		msg := translate.GetMessage(message, lang)
+		if msg != "" {
+			message = msg
+		}
+		data = map[string]interface{}{
+			"message": message,
+		}
+		res.Data = data
+	} else {
+		res.Data = data
+	}
+	ctx.Status(200)
+	getAPIStats(ctx.OriginalURL()).AddSuccess()
+	return prepareBody(ctx, res)
+}
+func Error(ctx *fiber.Ctx, section, errStr string, code int, msg ...string) error {
+	res := common.Response{}
+	message := ""
+	if len(msg) > 0 {
+		message = msg[0]
+	}
+	errCode := GetAPIBaseErrCode(ctx) + section + errStr
+	res.Status = "error"
+	lang := GetAcceptLanguage(ctx)
+
+	res.Error.Code = errCode
+	errMessage := translate.GetMessage(errCode, lang)
+	res.Error.Message = errMessage
+	if res.Error.Message == "" {
+		if message == "" {
+			res.Error.Message = translate.GetMessage(strconv.Itoa(code), lang)
+		} else {
+			msg := translate.GetMessage(message, lang)
+			if msg != "" {
+				res.Error.Message = msg
+			} else if code == 200 {
+				res.Error.Message = message
+			}
+		}
+		if res.Error.Message == "" {
+			res.Error.Message = translate.GetMessage(strconv.Itoa(500), lang)
+		}
+	}
+	ctx.Status(code)
+	getAPIStats(ctx.OriginalURL()).AddError()
+	return prepareBody(ctx, res)
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.17
 
 require (
 	github.com/go-playground/validator/v10 v10.9.0
+	github.com/go-redis/redis/v8 v8.11.4
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gofiber/fiber/v2 v2.22.0
 	github.com/json-iterator/go v1.1.12
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/prometheus/client_golang v1.11.0
@@ -15,11 +17,14 @@ require (
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/andybalholm/brotli v1.0.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -28,10 +33,13 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.26.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
