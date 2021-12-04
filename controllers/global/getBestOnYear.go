9b689fbe9e275d2d6611fa203ea96329d80ae215
HosseinZadeh <mrhb6006@gmail.com>
2021-12-04 21:56:32 +0330

    add get best on year controller

---
+++ controllers/global/getBestOnYear.go
@@ -0,0 +1,29 @@
+package global
+
+import (
+	"cloudProject/controllers/mainController"
+	gameRepo "cloudProject/models/game"
+	"cloudProject/pkg/cast"
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+func GetBestOnYear(ctx *fiber.Ctx) error {
+	spanCtx, traceID := mainController.InitAPI(ctx, "2003", "getBestOnYear")
+	defer mainController.FinishAPISpan(ctx)
+
+	year, _ := cast.ToInt(ctx.Query("year"))
+	if year == 0 {
+		return mainController.Error(ctx, "01", "01", 404)
+	}
+	NBest, _ := cast.ToInt(ctx.Query("N"))
+	if NBest == 0 {
+		return mainController.Error(ctx, "02", "01", 404)
+	}
+	games, errStr, err := gameRepo.Repo.GetBestOnYear(spanCtx, year, NBest)
+	if err != nil {
+		zap.L().Error("get best on year err", zap.String("traceID", traceID), zap.Error(err))
+		return mainController.Error(ctx, "03", errStr, 500)
+	}
+	return mainController.Response(ctx, games)
+}
