2961107fe5d8b10aef5e77937321d2ec0875362e
hossein <hoseyn.farahmandnezhad@gmail.com>
2021-12-06 00:00:29 +0330

    feat:implement annually sale with db

--- controllers/dataAnalytic/SumSellAnnually.go
+++ controllers/dataAnalytic/SumSellAnnually.go
@@ -2,25 +2,27 @@ package dataAnalytic
 
 import (
 	"cloudProject/controllers/mainController"
+	gameRepo "cloudProject/models/game"
+	"cloudProject/pkg/cast"
 	"cloudProject/pkg/draw"
 	"github.com/gofiber/fiber/v2"
-	"github.com/wcharczuk/go-chart/v2"
 	"go.uber.org/zap"
+	"strconv"
+	"time"
 )
 
 func SumSellAnnually(ctx *fiber.Ctx) error {
-	_, traceID := mainController.InitAPI(ctx, "50000", "genreSells")
+	spanCtx, traceID := mainController.InitAPI(ctx, "50000", "genreSells")
 	zap.L().Debug("sumSellAnnually_start", zap.String("traceID", traceID))
 	defer mainController.FinishAPISpan(ctx)
 
-	from := ctx.Query("from")
-	to := ctx.Query("to")
-	if from == "" || to == "" {
-		zap.L().Error("sumSellAnnually_params_invalid_err", zap.String("traceID", traceID))
-		return mainController.Error(ctx, "01", "producer not found", 404)
+	startYear, _ := cast.ToInt(ctx.Query("from", "1970"))
+	endYear, _ := cast.ToInt(ctx.Query("to", strconv.Itoa(time.Now().Year())))
+	data, errStr, err := gameRepo.Repo.GetYearBetweenYears(spanCtx, startYear, endYear)
+	if err != nil {
+		zap.L().Error("get best on year err", zap.String("traceID", traceID), zap.Error(err))
+		return mainController.Error(ctx, "01", errStr, 500)
 	}
-	//todo fetch data from main service
-	data := []chart.Value{}
 	bytes, err := draw.SumSellAnnually(data)
 	if err != nil {
 		zap.L().Error("sumSellAnnually_draw_err", zap.String("traceID", traceID))
