989e63cb42bf5d14f1a7a95436f768016803d894
hossein <hoseyn.farahmandnezhad@gmail.com>
2021-12-05 21:10:14 +0330

    feat:add route of new controllers

--- services/global/route.go
+++ services/global/route.go
@@ -1,4 +1,4 @@
-package global
+package main
 
 import (
 	. "cloudProject/controllers/global"
@@ -7,18 +7,22 @@ import (
 )
 
 var routes = map[string]string{
-	"getByRank":         "/global/get/byRank",
-	"getByName":         "/global/get/byName",
-	"getBestOnPlatform": "/global/get/bestOnPlatform",
-	"getBestOnYear":     "/global/get/bestOnYear",
-	"getBestOnGenre":    "/global/get/bestOnGenre",
+	"getByRank":                  "/global/get/byRank",
+	"getByName":                  "/global/get/byName",
+	"getBestOnPlatform":          "/global/get/bestOnPlatform",
+	"getBestOnYear":              "/global/get/bestOnYear",
+	"getBestOnGenre":             "/global/get/bestOnGenre",
+	"getBest5YearPlatform":       "/global/get/best5SellsByYearAndPlatform",
+	"europeMoreThanNorthAmerica": "/global/get/europeMoreThanNorthAmerica",
 }
 
 func setUpRoutes(app *fiber.App) {
-	app.Use(middlewares.Auth)
+	app.Use(middlewares.Auth())
 	app.Get(routes["getByRank"], GetByRank)
 	app.Get(routes["getByName"], GetByName)
 	app.Get(routes["getBestOnPlatform"], GetBestOnPlatform)
 	app.Get(routes["getBestOnYear"], GetBestOnYear)
 	app.Get(routes["getBestOnGenre"], GetBestOnGenre)
+	app.Get(routes["getBest5YearPlatform"], GetBest5SellsByYearAndPlatform)
+	app.Get(routes["europeMoreThanNorthAmerica"], GetEuropeMoreThanNorthAmerica)
 }
