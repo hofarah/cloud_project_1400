2c1db73d361c59716ed124344a33e82f840a0c27
hossein <hoseyn.farahmandnezhad@gmail.com>
2021-12-05 23:16:41 +0330

    feat:draw producer sells from db data from now

--- controllers/dataAnalytic/producerSells.go
+++ controllers/dataAnalytic/producerSells.go
@@ -2,29 +2,33 @@ package dataAnalytic
 
 import (
 	"cloudProject/controllers/mainController"
+	"cloudProject/models/game"
+	"cloudProject/pkg/cast"
 	"cloudProject/pkg/draw"
 	"github.com/gofiber/fiber/v2"
-	"github.com/wcharczuk/go-chart/v2"
 	"go.uber.org/zap"
 	"strconv"
 	"time"
 )
 
 func ProducerSells(ctx *fiber.Ctx) error {
-	_, traceID := mainController.InitAPI(ctx, "50000", "genreSells")
+	spanCtx, traceID := mainController.InitAPI(ctx, "50000", "genreSells")
 	zap.L().Debug("ProducerSells_start", zap.String("traceID", traceID))
 	defer mainController.FinishAPISpan(ctx)
-	startYear := ctx.Query("from", "1970")
-	endYear := ctx.Query("to", strconv.Itoa(time.Now().Year()))
+	startYear, _ := cast.ToInt(ctx.Query("from", "1970"))
+	endYear, _ := cast.ToInt(ctx.Query("to", strconv.Itoa(time.Now().Year())))
 	producer1 := ctx.Query("p1")
 	producer2 := ctx.Query("p2")
 	if producer1 == "" || producer2 == "" {
 		zap.L().Error("ProducerSells_params_invalid_err", zap.String("traceID", traceID))
 		return mainController.Error(ctx, "01", "producer not found", 404)
 	}
-	//todo fetch data from main service
-	data := map[string][]chart.Value{}
-	bytes, err := draw.ProducerSells(startYear, endYear, data)
+	data, errStr, err := game.Repo.GetProducerOnYears(spanCtx, producer1, producer2, startYear, endYear)
+	if err != nil {
+		zap.L().Error("get producer on years failed", zap.String("traceID", traceID), zap.Error(err))
+		return mainController.Error(ctx, "02", errStr, 500)
+	}
+	bytes, err := draw.ProducerSells(data)
 	if err != nil {
 		zap.L().Error("genreSells_draw_err", zap.String("traceID", traceID))
 		return mainController.Error(ctx, "01", err.Error(), 500)
